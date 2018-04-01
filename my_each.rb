7f7f4bfb650598593ee2354743afc099c74a897d
Shawn Koski <shawnnkoski@gmail.com>
2018-04-01 18:32:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  i = 0
+  
+  while i < collection.size
+    puts "#{colection[i]}"
+    i += 1
+  end
 end
